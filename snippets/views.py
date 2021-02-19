7789c3a5a1e090512c9a11c7ab1a9ff52402ed47
이혜성 <hyeseong43@gmail.com>
2021-02-19 20:27:15 +0900

    Tutorial 4: Authentication & Permissions 1) Adding information to our model 2) Adding endpoints for our User models 3) Associating Snippets with Users 4) Updating our serializer 5) Adding required permissions to views 6) Adding login to the Browsable API 7) Object level permissions 8) Authenticating with the API

--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,31 @@
+from django.contrib.auth.models import User
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from rest_framework import permissions
+from snippets import serializers  
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+    serializer_class = serializers.SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+    serializer_class = serializers.SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                            IsOwnerOrReadOnly]
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
